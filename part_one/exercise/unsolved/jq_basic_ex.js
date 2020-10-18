9ea366c80f3063a92b46630bf228d0567b839d15
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-10-18 09:27:15 -0400

    adding exercise

---
+++ part_one/exercise/unsolved/jq_basic_ex.js
@@ -0,0 +1,8 @@
+/*
+
+1. import jquery
+2. make the image appear after you click the appropriate button
+3. make the font color of the h2 tag to change to blue on click
+4. When the approprite button is clicked, add onto the respective div with a p tag that has your name as text
+
+*/
